496946e9c5a9cfc41324b4ae7036210f7c12dfe0
Tejas Pancholi <tejas.pancholi@deepshieldai.com>
2025-06-18 23:08:58 +0530

    :sparkles: complete starter template

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import checkFile from "eslint-plugin-check-file";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -23,10 +24,32 @@ const eslintConfig = [
       "prefer-template": ["error"],
       "semi": ["error"],
       "quotes": ["error", "double"]
-    }
+    },
   }
-
-  )
+  ),
+  {
+    plugins: {
+      "check-file": checkFile,
+    },
+    files: ["src/**/*"],
+    rules: {
+        "check-file/filename-naming-convention": [
+          "error",
+          {
+            "**/*.{ts,tsx}": "KEBAB_CASE",
+          },
+          {
+            ignoreMiddleExtensions: true,
+          },
+        ],
+        "check-file/folder-naming-convention": [
+          "error",
+          {
+            "src/**": "KEBAB_CASE"
+          },
+        ],
+    },
+  },
 ];
 
 export default eslintConfig;
